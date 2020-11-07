be7862ea4db8a9d66cb5d034b3e9dc55e3267d9d
xhsun1997 <sun64410822@163.com>
2020-11-07 16:35:16 +0800

    f1 approximately equal to 0.74

--- data_process.py
+++ data_process.py
@@ -4,74 +4,88 @@ import torch
 from collections import Counter
 
 def read_data(json_files,word_limit_freq=10):
-	if type(json_files)!=list:
-		json_files=[json_files]
-	examples=[]
-	all_words=[]
-	unk_example_nums=0
-	for json_file in json_files:
-		with open(json_file) as f:
-			lines=f.readlines()
-		for line in lines:
-			example=json.loads(line.strip())
-			if example["answer"]["text"]=="null" or example["answer"]["start_position"]==-1:
-				example["answer"]['end_position']=-1
-				unk_example_nums+=1
-			else:
-				example["answer"]["end_position"]=len(str(example["answer"]["text"]))+example["answer"]["start_position"]
-			context=example["context"]
-			assert type(context)==str
-			for word in context:
-				all_words.append(word)
-			all_words.append(char for char in example["context"])
-			examples.append(example)
-	counter_words=Counter(all_words)
-	sorted_words=sorted(counter_words.items(),key=lambda x:x[1],reverse=True)
-	word2id={"[PAD]":0,"[UNK]":1}
-	for word,word_freq in sorted_words:
-		if word_freq>word_limit_freq:
-			word2id[word]=len(word2id)
-	print("unknown question examples : %d , total examples : %d"%(unk_example_nums,len(examples)))
-	return examples,word2id
+    if type(json_files)!=list:
+        json_files=[json_files]
+    examples=[]
+    eval_examples={}
+    all_words=[]
+    unk_example_nums=0
+    unique_qa_id=0
+    for json_file in json_files:
+        with open(json_file) as f:
+            lines=f.readlines()
+        for line in lines:
+            example=json.loads(line.strip())
+            if len(example["context"])<=1 or len(example["question"])<=1:
+                print("bad example : ",example)
+                continue
+
+            if example["answer"]["text"]=="null" or example["answer"]["start_position"]==-1:
+                #example["answer"]['end_position']=-1
+                unk_example_nums+=1
+                continue#最初不考虑无答案问题，循序渐进
+            else:
+                example["answer"]["end_position"]=len(str(example["answer"]["text"]))+example["answer"]["start_position"]
+            context=example["context"]
+            assert type(context)==str
+            for word in context:
+                all_words.append(word)
+            all_words.append(char for char in example["context"])
+            example["unique_qa_id"]=unique_qa_id
+            eval_examples[str(unique_qa_id)]={"context":context,
+                                            "spans":[example["answer"]["start_position"],example["answer"]["end_position"]],
+                                            "answer_text":example["answer"]["text"]}
+            unique_qa_id+=1
+            examples.append(example)
+    counter_words=Counter(all_words)
+    sorted_words=sorted(counter_words.items(),key=lambda x:x[1],reverse=True)
+    word2id={"[PAD]":0,"[UNK]":1}
+    for word,word_freq in sorted_words:
+        if word_freq>word_limit_freq:
+            word2id[word]=len(word2id)
+    print("unknown question examples : %d , total examples : %d"%(unk_example_nums,len(examples)))
+    return examples,word2id,eval_examples
 
 def convert_to_ids(examples,word2id):
-	features=[]
-	def get_word_id(word):
-		if word in word2id:
-			return word2id[word]
-		else:
-			return word2id["[UNK]"]
+    features=[]
+    def get_word_id(word):
+        if word in word2id:
+            return word2id[word]
+        else:
+            return word2id["[UNK]"]
 
-	for example in examples:
-		context=example["context"]
-		question=example["question"]
-		context_ids=[get_word_id(word) for word in context]
-		question_ids=[get_word_id(word) for word in question]
-		features.append({"context_ids":context_ids,"question_ids":question_ids,
-						"start_position":example["answer"]["start_position"],
-						"end_position":example["answer"]["end_position"]})
-	return features
+    for example in examples:
+        qa_id=example["unique_qa_id"]
+        context=example["context"]
+        question=example["question"]
+        context_ids=[get_word_id(word) for word in context]
+        question_ids=[get_word_id(word) for word in question]
+        features.append({"unique_qa_id":qa_id,"context_ids":context_ids,"question_ids":question_ids,
+                        "start_position":example["answer"]["start_position"],
+                        "end_position":example["answer"]["end_position"]})
+    return features
 
 def convert_to_words(features,word2id):
-	examples=[]
-	id2word={id_:word for word,id_ in word2id.items()}
-	if type(features)!=list:
-		features=[features]
-	for feature in features:
-		context_ids=feature["context_ids"]
-		question_ids=feature["question_ids"]
-		start_position=feature["start_position"]
-		end_position=feature["end_position"]
-		context=""
-		for id_ in context_ids:
-			context+=id2word[id_]
-		question=""
-		for id_ in question_ids:
-			question+=id2word[id_]
-		answer=context[feature["start_position"]:feature["end_position"]]
-		examples.append({"context":context,"question":question,
-						"answer":{"text":answer,"start_position":feature["start_position"],"end_position":feature["end_position"]}})
-	return examples
+    examples=[]
+    id2word={id_:word for word,id_ in word2id.items()}
+    if type(features)!=list:
+        features=[features]
+    for feature in features:
+        qa_id=feature["unique_qa_id"]
+        context_ids=feature["context_ids"]
+        question_ids=feature["question_ids"]
+        start_position=feature["start_position"]
+        end_position=feature["end_position"]
+        context=""
+        for id_ in context_ids:
+            context+=id2word[id_]
+        question=""
+        for id_ in question_ids:
+            question+=id2word[id_]
+        answer=context[feature["start_position"]:feature["end_position"]]
+        examples.append({"unique_qa_id":qa_id,"context":context,"question":question,
+                        "answer":{"text":answer,"start_position":feature["start_position"],"end_position":feature["end_position"]}})
+    return examples
 
 
 # class DataSet(torch.utils.data.DataSet):
@@ -89,58 +103,60 @@ def convert_to_words(features,word2id):
 # 	def __getitem__(self,idx):
 
 class Config:
-	def __init__(self):
-		self.train_question_len_limit=48
-		self.train_context_len_limit=448
-		self.test_question_len_limit=48
-		self.test_context_len_limit=512
+    def __init__(self):
+        self.train_question_len_limit=48
+        self.train_context_len_limit=448
+        self.test_question_len_limit=48
+        self.test_context_len_limit=512
 config=Config()
 
 def pad_features(features,config,is_train=True):
-	'''
-	features的每一个值是一个dict，keys=={"context_ids","question_ids","start_position","end_position"}
-	函数的目的是将每一个样本的context_ids和question_ids补全到context_len_limit和question_len_limit
-	'''
-	def pad_ids(obj_ids,max_seq_len):
-		if len(obj_ids)>=max_seq_len:
-			obj_ids=obj_ids[:max_seq_len]
-		else:
-			obj_ids+=[0]*(max_seq_len-len(obj_ids))
-		return obj_ids
+    '''
+    features的每一个值是一个dict，keys=={"context_ids","question_ids","start_position","end_position"}
+    函数的目的是将每一个样本的context_ids和question_ids补全到context_len_limit和question_len_limit
+    '''
+    def pad_ids(obj_ids,max_seq_len):
+        if len(obj_ids)>=max_seq_len:
+            obj_ids=obj_ids[:max_seq_len]
+        else:
+            obj_ids+=[0]*(max_seq_len-len(obj_ids))
+        return obj_ids
 
-	context_len_limit=config.train_context_len_limit if is_train else config.test_context_len_limit
-	question_len_limit=config.train_question_len_limit if is_train else config.test_question_len_limit
-	deprected_example_nums=0
-	new_features=[]
-	for feature in features:
-		context_ids=feature["context_ids"]
-		question_ids=feature["question_ids"]
-		start_position=feature["start_position"]
-		end_position=feature["end_position"]
+    context_len_limit=config.train_context_len_limit if is_train else config.test_context_len_limit
+    question_len_limit=config.train_question_len_limit if is_train else config.test_question_len_limit
+    deprected_example_nums=0
+    new_features=[]
+    for feature in features:
+        context_ids=feature["context_ids"]
+        question_ids=feature["question_ids"]
+        start_position=feature["start_position"]
+        end_position=feature["end_position"]
+        unique_qa_id=feature["unique_qa_id"]
 
-		if start_position>=context_len_limit or end_position>=context_len_limit:
-			deprected_example_nums+=1
-			continue
+        if start_position>=context_len_limit or end_position>=context_len_limit:
+            deprected_example_nums+=1
+            continue
 
-		context_ids=pad_ids(context_ids,context_len_limit)
-		question_ids=pad_ids(question_ids,question_len_limit)
-		new_features.append({"context_ids":context_ids,"question_ids":question_ids,"start_position":start_position,"end_position":end_position})
-	print("total feature nums : %d , deprected_example_nums : %d , rest features : %d"%(len(features),deprected_example_nums,len(new_features)))
-	return new_features
+        context_ids=pad_ids(context_ids,context_len_limit)
+        question_ids=pad_ids(question_ids,question_len_limit)
+        new_features.append({"unique_qa_id":unique_qa_id,"context_ids":context_ids,"question_ids":question_ids,"start_position":start_position,"end_position":end_position})
+    print("total feature nums : %d , deprected_example_nums : %d , rest features : %d"%(len(features),deprected_example_nums,len(new_features)))
+    return new_features
 
 def get_dataloader(features,batch_size,is_train=True):
-	all_context_ids=torch.tensor([feature["context_ids"] for feature in features],dtype=torch.long)
-	all_question_ids=torch.tensor([feature["question_ids"] for feature in features],dtype=torch.long)
-	all_start_positions=torch.tensor([feature["start_position"] for feature in features],dtype=torch.long)
-	all_end_positions=torch.tensor([feature["end_position"] for feature in features],dtype=torch.long)
+    all_unique_qa_ids=torch.tensor([feature["unique_qa_id"] for feature in features],dtype=torch.long)
+    all_context_ids=torch.tensor([feature["context_ids"] for feature in features],dtype=torch.long)
+    all_question_ids=torch.tensor([feature["question_ids"] for feature in features],dtype=torch.long)
+    all_start_positions=torch.tensor([feature["start_position"] for feature in features],dtype=torch.long)
+    all_end_positions=torch.tensor([feature["end_position"] for feature in features],dtype=torch.long)
 
-	dataset=torch.utils.data.TensorDataset(all_context_ids,all_question_ids,all_start_positions,all_end_positions)
-	if is_train:
-		sampler=torch.utils.data.RandomSampler(dataset)
-	else:
-		sampler=torch.utils.data.SequentialSampler(dataset)
-	dataloader=torch.utils.data.DataLoader(dataset,sampler=sampler,batch_size=batch_size)
-	return dataloader
+    dataset=torch.utils.data.TensorDataset(all_unique_qa_ids,all_context_ids,all_question_ids,all_start_positions,all_end_positions)
+    if is_train:
+        sampler=torch.utils.data.RandomSampler(dataset)
+    else:
+        sampler=torch.utils.data.SequentialSampler(dataset)
+    dataloader=torch.utils.data.DataLoader(dataset,sampler=sampler,batch_size=batch_size)
+    return dataloader
 
 '''
 for i,batch_data in enumerate(dataloader):
--- main.py
+++ main.py
@@ -1,27 +1,178 @@
+import os
 import argparse
 import torch
 import torch.nn as nn
 from BiDAF import BiDAF
 from data_process import *
+from utils import get_metrics
+import pickle
+from tensorboardX import SummaryWriter
+import random
 
-def train(config,train_dataloader):
-	device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	model=BiDAF(config)
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device : ",device)
 
-	optimizer=torch.optim.Adam(model.parameters(),lr=config.learning_rate)
-	optimizer.zero_grad()
-	criterion=nn.CrossEntropyLoss()
+#sed -n "line1,line2p" file | tee target_file
+def train(config,train_dataloader,test_dataloader,eval_examples):
+    writer=SummaryWriter(log_dir=config.log_dir)
+    model=BiDAF(config)
+    optimizer=torch.optim.Adam(model.parameters(),lr=config.learning_rate)
+    scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,"min",factor=0.8,patience=3,verbose=True)
+    #patience是指，如果第四个epochs都没有下降的话，那么new_lr=factor*orig_lr
+    criterion=nn.CrossEntropyLoss()
 
-	model.train()
-	loss=0
-	for epoch in range(config.epochs):
+    model.to(device)
+    model.train()
+    loss=0
+    best_f1_score=0
+    training_loss_record=[]
+    test_loss_record=[]
+    test_f1_record=[]
+    test_em_record=[]
 
-		for step,batch_data in enumerate(train_dataloader):
-			start_positions,end_positions=batch_data[-2],batch_data[-1]
-			start_logits,end_logits=model(batch_data)
-			loss=criterion(start_logits,start_positions)+criterion(end_logits,end_positions)
-			print("loss : ",loss)
-			loss.backward()
-			optimizer.step()
-			optimizer.zero_grad()
+    for epoch in range(config.epochs):
+
+        for step,batch_data in enumerate(train_dataloader):
+            batch_qa_ids,context_ids,question_ids,start_positions,end_positions=batch_data
+            start_logits,end_logits=model(context_ids,question_ids)
+
+            optimizer.zero_grad()
+
+            batch_loss=0.5*criterion(start_logits,start_positions.to(device))+0.5*criterion(end_logits,end_positions.to(device))
+            loss+=batch_loss.item()
+            batch_loss.backward()
+            optimizer.step()
+
+            #if (step+1)%config.gradient_accumulation_steps==0:
+            if (step+1)%config.print_loss_step==0:
+                print("epoch : %d , step : %d , loss %.3f"%(epoch,step,loss/(config.print_loss_step)))
+                training_loss_record.append(loss/config.print_loss_step)
+                writer.add_scalar("loss/train",loss/config.print_loss_step,(step+1)//config.print_loss_step)
+                loss=0
+
+            if (step+1)%config.test_step==0:
+                test_loss,f1_score,em_value=test(model,config,test_dataloader,eval_examples)
+
+                writer.add_scalar("loss/test",test_loss,(step+1)//config.test_step)
+                writer.add_scalar("em/test",em_value,(step+1)//config.test_step)
+                writer.add_scalar("f1/test",f1_score,(step+1)//config.test_step)
+
+                test_loss_record.append(test_loss)
+                test_f1_record.append(f1_score)
+                test_em_record.append(em_value)
+
+                scheduler.step(test_loss)#监控test_loss
+                print("epoch : %d , step : %d , learning rate : %f , test loss : %.3f , f1_score : %.3f , em_value : %.3f"%(epoch,step,optimizer.param_groups[0]["lr"],test_loss,f1_score,em_value))
+                if f1_score>best_f1_score:
+                    output_model_file=os.path.join(config.save_model_path,
+                                                "model"+str(f1_score).split(".")[1][:3]+".bin")
+                    torch.save(model.state_dict(),output_model_file)
+                    print("Save model to ",output_model_file)
+                    best_f1_score=f1_score
+                model.train()
+    with open("./log.pkl","wb") as f:
+        obj=[training_loss_record,test_loss_record,test_f1_record,test_em_record]
+        pickle.dump(obj,f)
+    writer.close()
+
+
+def test(model,config,test_dataloader,eval_examples):
+    criterion=nn.CrossEntropyLoss()
+    model.eval()
+    loss=0
+    total_ems=[]
+    total_f1_scores=[]
+    with torch.no_grad():
+        for step,batch_data in enumerate(test_dataloader):
+            batch_qa_ids,context_ids,question_ids,start_positions,end_positions=batch_data
+            start_logits,end_logits=model(context_ids,question_ids)
+
+            ignore_index=start_logits.size(1)
+            start_positions.clamp_(0,ignore_index)
+            end_positions.clamp_(0,ignore_index)
+            start_loss=nn.CrossEntropyLoss(ignore_index=ignore_index)(start_logits,start_positions.to(device))
+            end_loss=nn.CrossEntropyLoss(ignore_index=ignore_index)(end_logits,end_positions.to(device))
+            loss+=(0.5*start_loss+0.5*end_loss).item()
+
+            batch_size,context_len=start_logits.size()
+            log_softmax=nn.LogSoftmax(dim=1)#在context_len维度上做log_softmax
+            mask=(torch.ones(context_len,context_len)*float('-inf')).to(device).tril(-1).unsqueeze(0).expand(batch_size,-1,-1)
+            #mask是batch_size个形状为(context_len,context_len)的下三角矩阵，对角线以及上三角部分为0
+            score=log_softmax(start_logits).unsqueeze(2)+log_softmax(end_logits).unsqueeze(1)+mask
+
+            # score,start_predictions=score.max(dim=1)
+            # score,end_predictions=score.max(dim=1)
+            start_pred_ids=torch.argmax(torch.max(score,dim=2)[0],dim=1).tolist()
+            end_pred_ids=torch.argmax(torch.max(score,dim=1)[0],dim=1).tolist()
+
+            batch_ems,batch_f1_scores=get_metrics(start_pred_ids,end_pred_ids,batch_qa_ids,eval_examples)
+            #batch_ems记录着batch_size个样本的em值，batch_f1_scores记录着batch_size个样本的f1值
+            total_ems.extend(batch_ems)
+            total_f1_scores.extend(batch_f1_scores)
+
+    f1_score=sum(total_f1_scores)/len(total_f1_scores)
+    em_value=sum(total_ems)/len(total_ems)
+    #print("test f1_score : %.3f , test em value : %.3f"%(f1_score,em_value))
+    return loss/step,f1_score,em_value
+
+
+class Config:
+    def __init__(self):
+        self.train_question_len_limit=48
+        self.train_context_len_limit=512
+        self.test_question_len_limit=48
+        self.test_context_len_limit=512
+        #self.vocab_size=len(word2id)
+        self.embed_dim=300
+        self.highway_network_layers=2
+        self.hidden_size=150
+        self.dropout=0.2
+        self.learning_rate=0.001
+        self.epochs=20
+        self.gradient_accumulation_steps=1
+        self.print_loss_step=100
+        self.data_folder="./data"
+        self.files_name=["my_cmrc2018.json","my_dureader.json","my_military.json","my_cail2019.json"]
+        self.train_batch_size=48
+        self.test_batch_size=48
+        self.test_step=300
+        self.save_model_path="./saved_best_model/"
+        self.log_dir="./log_dir/"
+
+    def add_vocab_size(self,vocab_size):
+        self.vocab_size=vocab_size
+def main():
+    json_files=[]
+    config=Config()
+    
+    for file_name in config.files_name:
+        json_files.append(os.path.join(config.data_folder,file_name))
+    examples,word2id,eval_examples=read_data(json_files)#我们根据qa_id就可以得到对应的example
+    random.shuffle(examples)#打乱数据
+    config.add_vocab_size(len(word2id))
+    total_examples_nums=len(examples)
+    train_examples=examples[:int(total_examples_nums*0.8)]
+    test_examples=examples[int(total_examples_nums*0.8):]
+
+    train_features=convert_to_ids(train_examples,word2id)
+    test_features=convert_to_ids(test_examples,word2id)
+    #convert_to_ids并不改变examples和features之间的关系
+    #pad_features可能会过滤掉一些example，但是我们利用unique_qa_id记住了每一个qa的id，
+    #所以features里面的unique_qa_id和examples里面的unique_qa_id是可以对应的
+    #只不过len(features)<=len(examples)
+
+    train_features=pad_features(train_features,config)
+    test_features=pad_features(test_features,config,is_train=False)
+
+    print("Number of train : %d , number of test : %d "%(len(train_features),len(test_features)))
+
+    train_dataloader=get_dataloader(train_features,batch_size=config.train_batch_size)
+    test_dataloader=get_dataloader(test_features,batch_size=config.test_batch_size,is_train=False)
+
+    train(config,train_dataloader=train_dataloader,test_dataloader=test_dataloader,eval_examples=eval_examples)
+
+
+main()
+
+
 
